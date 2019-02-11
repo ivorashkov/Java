3a70b4f35b9a783060b11401793f395173a01e45
ivorashkov <47078490+ivorashkov@users.noreply.github.com>
2019-02-11 22:25:49 +0200

    Update StudentDatabaseApp.java

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -5,22 +5,28 @@ public class StudentDatabaseApp
 {
     public static void main(String[] args)
     {
+         //Ask how many new students we want to add
+        StudentData stu1 = new StudentData();
+        stu1.enroll();
+        stu1.payTuition();
+        System.out.println(stu1.toString());
 
-        //Ask how many new students we want to add
-        System.out.println("Enter number of new students to enroll:");
-        Scanner in = new Scanner(System.in);
-        int numOfStudents = in.nextInt();
+
+//         //Ask how many new students we want to add
+//         System.out.println("Enter number of new students to enroll:");
+//         Scanner in = new Scanner(System.in);
+//         int numOfStudents = in.nextInt();
 
 
-        //Create n number of new students
-        for (int n = 0; n < numOfStudents; n++){
-            StudentData stu = new StudentData();
+//         //Create n number of new students
+//         for (int n = 0; n < numOfStudents; n++){
+//             StudentData stu = new StudentData();
 //            stu = students[n];
 //            students[n].stu.enroll(); ;
 //            students[n].();
 //            students.payTuition();
 //            System.out.println(students.toString());
-        }
+      //  }
 
     }
 }
